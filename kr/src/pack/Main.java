673ccbc3ce57eed0e3c223c44b70534a67072d1c
Silvanika-Jakon <103750658+Silvanika-Jakon@users.noreply.github.com>
2022-12-19 17:37:48 +0300

    Update Main.java

--- kr/src/pack/Main.java
+++ kr/src/pack/Main.java
@@ -1,20 +1,15 @@
 package pack;
+import java.text.MessageFormat;
+import java.util.*;
 
-import java.util.Scanner;
+import static pack.print.print_color;
 
-public class print {
-    public static void print_color(String color, String text) {
-        Color e = check(color);
-        System.out.println(e + text + Color.RESET);
-    }
-    public static Color check(String id) {
-        try {
-            return Color.valueOf(id);
-        } catch (IllegalArgumentException e) {
-            throw new RuntimeException(
-                    "Invalid value for color");
+public class Main {
+
+    public static void main(String[] args) {
+        if (true) {
+            print_color("GREE", "some text");
         }
-
+        }
     }
-}
 
