b5b471311b6d4e40968dbb97e48fb5c67f3997a9
Silvanika-Jakon <103750658+Silvanika-Jakon@users.noreply.github.com>
2022-12-23 19:22:50 +0300

    Update Main.java

--- kr/src/pack/Main.java
+++ kr/src/pack/Main.java
@@ -1,15 +1,11 @@
 package pack;
-import java.text.MessageFormat;
-import java.util.*;
 
 import static pack.print.print_color;
 
 public class Main {
 
     public static void main(String[] args) {
-        if (true) {
             print_color("GREE", "some text");
-        }
         }
     }
 
